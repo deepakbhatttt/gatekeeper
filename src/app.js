35dd7219953528bb84b12dab982e273569cc133c
Deepak Bhatt <70554431+deepakbhatttt@users.noreply.github.com>
2024-03-11 00:06:31 +0530

    Update app.js Route Render

--- src/app.js
+++ src/app.js
@@ -35,4 +35,5 @@ const appRouter = createBrowserRouter([
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter} />);
+//root.render(<RouterProvider router={appRouter} />);
+root.render(<AppLayout/>);
